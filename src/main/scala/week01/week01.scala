5755b2e2cf35aeeb67c51b89d9374966aac8336d
Robert Piro <r.e.f.piro@gmail.com>
2024-11-04 04:43:32 +0000

    implement gcd

--- src/main/scala/week01/week01.scala
+++ src/main/scala/week01/week01.scala
@@ -4,6 +4,7 @@ object Week01 {
     def main(args:Array[String]): Unit = 
         val sqrt_2 = newton_raphson(x=> x*x-2, x=> 2*x, 3)
         println(sqrt_2)
+        println(gcd(15, 27))
 
 
     def newton_raphson(f:Double => Double, fprime: Double => Double, start_value:Double, iter:Int =1000):Double =
@@ -13,5 +14,6 @@ object Week01 {
             val x = start_value - (f(start_value)/fprime(start_value))
             newton_raphson(f, fprime, x, iter -1)
 
-    
+    def gcd(x:Int, y:Int):Int =
+        if y == 0 then x else gcd(y, x%y)
 }
