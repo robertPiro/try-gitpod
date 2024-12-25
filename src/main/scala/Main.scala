4140eb3fcac97038654b5e1bb378eb59cb80172a
Robert Piro <r.e.f.piro@gmail.com>
2024-12-25 21:13:44 +0000

    implement bar chart

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -12,7 +12,8 @@ object Main:
     def appElement(): Element = 
         div(
             h1("Hello Scala.js, Vite and Laminar!"),
-            renderDataTable
+            renderDataTable,
+            renderDataGraph
         )
     //Var: mutible container for (ideally immutable) data to be propagated thru Laminar   
     val dataVar = Var(List(DataItem("one", 1.0)))
@@ -41,7 +42,60 @@ object Main:
             ),
             tfoot(tr(td(button0)))
         )
+    
+    def renderDataGraph: HtmlElement =
+        import typings.chartJs.mod.* 
+        import scala.scalajs.js.JSConverters.iterableOnceConvertible2JSRichIterableOnce
+        var optChart: Option[Chart] = None
 
+        //function to fill in the chart data
+        def fillInChartData(optChart:Option[Chart])(xyData:List[DataItem]) = 
+            for chart <- optChart 
+            do
+                chart.data.labels = xyData.map(_.label).toJSArray
+                chart.data.datasets.get(0).data = xyData.map(_.value).toJSArray
+                chart.update()
+
+        val chartConf = new ChartConfiguration{
+            `type`= ChartType.bar
+            data = new ChartData {
+                datasets = js.Array(
+                    new ChartDataSets {
+                        label = "Price"
+                        borderWidth = 1
+                        backgroundColor = "green"
+                    },
+                    new ChartDataSets {
+                        label = "Full price"
+                        borderWidth = 1
+                        backgroundColor = "blue"
+                    }
+                )
+            }
+        }
+        canvasTag(
+            width := "100%",
+            height := "200px",
+            onMountUnmountCallback(
+                mount = { nodeCtx => 
+                    val ctx = nodeCtx.thisNode.ref
+                    val chart = Chart.apply.newInstance2(ctx, chartConf)
+                    optChart = Some(chart) // defined as var above 
+                },
+                unmount = { thisNode => 
+                    for chart <- optChart
+                    do
+                        chart.destroy()
+                    optChart = None // defined as var above
+                }
+            ),
+            dataSignal --> {xyData => fillInChartData(optChart)(xyData)} //extracts data from signal
+
+                  
+        )
+    end renderDataGraph
+
+    
 final class DataID  //will create many instances that are compared by address
 trait DomRefObj(val id: DataID)
 case class DataItem(label: String, value: Double) extends DomRefObj(DataID())
